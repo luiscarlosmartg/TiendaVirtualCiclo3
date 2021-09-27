807b486ae72e1f5b6af89b5a4da98ebfe248d45b
luiscarlosmartg <luiscarlosmartg@gmail.com>
2021-09-26 20:39:19 -0500

    Agregades variables

--- src/holita/EjemploGit.java
+++ src/holita/EjemploGit.java
@@ -1,7 +1,9 @@
 package holita;
 
 public class EjemploGit {
-  public static void main(String[] args) {
-	  System.out.println("Hola alumnos");
-}
+	public static void main(String[] args) {
+		System.out.println("Hola alumnos");
+
+		String nombre = "Juan";
+	}
 }
